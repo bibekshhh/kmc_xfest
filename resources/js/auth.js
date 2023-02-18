73a53aebfd75a30a7ac05cf70b32294ea0f6e182
yaman1337 <yaman.sarabariya@gmail.com>
2023-02-18 16:52:47 +0545

    fixed routing

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,5 +1,5 @@
 const login = document.querySelector("#login");
 
 login.addEventListener("click", () => {
-    window.location.assign("http://127.0.0.1:5500/kmc_xfest/pages/login.html")
+    window.location.assign("/pages/login.html")
 })
